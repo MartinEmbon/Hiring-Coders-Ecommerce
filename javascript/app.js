a87cb9a8f1e92b762b6a9ab5b9e0851115339584
Martincho <martinembon@hotmail.com>
2021-07-13 19:13:08 -0300

    HC v2.0

--- javascript/app.js
+++ javascript/app.js
@@ -2,20 +2,21 @@ const inputEmail = document.getElementById("inputEmail")
 const btn = document.getElementById("btn")
 
 
-btn.addEventListener("click",()=>{
+btn.addEventListener("click",()=>{    
     const key="email"
     const value=inputEmail.value;
 
-    if(value){
-        localStorage.setItem(key,value)
-        console.log(key)
-        console.log(value)
+    if(key && value){        
+        localStorage.setItem(key,value)            
         inputEmail.value=" "
         alert(`Proceso de localStorage com chave ${key}:${value} feito com sucesso!`)    
-        location.reload()
         
-    }
-    
-    
+        for (let i=0;i <localStorage.length; i++){
+            const key=localStorage.key(i);
+            const value=localStorage.getItem(key)
+            console.log(value)
+            location.reload()
+        }
 
+    }
 })
